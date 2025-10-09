edcbd360236e2874fe11b8c1b7226097acf3863a
Dnyaneshwar S Jambhulkar <dnyanesh@Dnyaneshwars-MacBook-Pro.local>
2025-10-10 01:05:28 +0530

    AC-15351::Replacing Zend_Cache with symfony/cache component

--- dev/tests/integration/testsuite/Magento/Backend/controllers/_files/cache/non_application_cache.php
+++ dev/tests/integration/testsuite/Magento/Backend/controllers/_files/cache/non_application_cache.php
@@ -9,5 +9,5 @@ $cachePool = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
     ->create(\Magento\Framework\App\Cache\Frontend\Pool::class);
 /** @var $cacheFrontend \Magento\Framework\Cache\FrontendInterface */
 foreach ($cachePool as $cacheFrontend) {
-    $cacheFrontend->getBackend()->save('non-application cache data', 'NON_APPLICATION_FIXTURE', ['SOME_TAG']);
+    $cacheFrontend->save('non-application cache data', 'NON_APPLICATION_FIXTURE', ['SOME_TAG']);
 }
