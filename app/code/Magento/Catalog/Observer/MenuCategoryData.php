80a8a30282279ae1983c9a1787c8ddb1c9285f9e
Dan Wallis <dan@wallis.nz>
2025-12-16 21:38:02 +0000

    Add class to avoid breaking backward compatibility

---
+++ app/code/Magento/Catalog/Observer/MenuCategoryData.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\Catalog\Observer;
+
+use Magento\Catalog\Model\MenuCategoryData as MenuCategoryDataReal;
+
+/**
+ * @deprecated This class exists only to avoid breaking backwards compatibility.
+ * @see Magento\Catalog\Model\MenuCategoryData
+ */
+class MenuCategoryData extends MenuCategoryDataReal
+{
+}
